1002176b183f1269f1f81c51550608fd4674298c
Utsav Kuinkel <utshaav583@gmail.com>
2021-03-24 21:06:34 +0545

    Created a simple console app for client

---
+++ ClientApp/CommanderClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommanderClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
